f320465850cd52b7dd86719fe64538b11c0d2234
Tanmay Patwardhan <tanmay.xvii@gmail.com>
2018-08-18 22:43:11 -0700

    DOne with today's work

---
+++ python-august/trig.py
@@ -0,0 +1,7 @@
+import math
+list_of_degrees=[30.0,45.0,90.0,180.0,270.0,360.0]
+for deg in list_of_degrees:
+    rad=math.radians(deg)
+    print (("sin of ") + str(deg) + (" is ") +  str(math.sin(rad))  )
+    print (("cos of ") + str(deg) + (" is ") +  str(math.cos(rad))  )
+    print (("tan of ") + str(deg) + (" is ") +  str(math.tan(rad))  )
